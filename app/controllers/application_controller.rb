31f3548be78bdd19d0f61af42b1298b98a4c4cb3
Revath S Kumar <rsk@revathskumar.com>
2012-10-23 02:30:52 +0530

    adding require authentication method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # Forces user to authenticate with Instagram OAuth before allowing access to interface elements.
+  def require_authentication
+    unless session[:authenticated]
+      if Rails.env.production?
+        redirect_to "#{view_context.base_url}/auth/instagram"
+      else
+        redirect_to "#{view_context.base_url}/auth/developer"
+      end
+    end
+  end
 end
