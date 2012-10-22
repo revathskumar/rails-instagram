cb4f12f34f61f799afcaca1e8c8ccc482da40cf0
Revath S Kumar <rsk@revathskumar.com>
2012-10-23 00:38:57 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InstaApp::Application.config.secret_token = '87deb64f5b04027991f0a489b917d9abcffa1e26b8eff2d5203396f5189fab66293363235ca8d5c3ae8584fa237287fc7ce4e080c19151f370e9b65b0cd4afe9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+InstaApp::Application.config.session_store :cookie_store, key: '_InstaApp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# InstaApp::Application.config.session_store :active_record_store
